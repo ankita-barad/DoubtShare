f46d34c40527a1eb635ad14ea65241f0622425ec
ankita-barad <ankitabarad2111@gmail.com>
2024-01-06 10:09:37 +0530

    try deploy fix

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import doubtRouter from "./routes/doubt.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 
 app.use("/user", userRouter);
 app.use("/subject", subjectRouter);
